2da4248f77223a9e45a30984570eafa85adc6b25
TUANHUNG PHAN <fatenumber25@TUANHUNGnoiMac.local>
2021-10-11 07:40:51 +0900

    first commit

---
+++ Sources/SambaBridge/SambaBridge.swift
@@ -0,0 +1,6 @@
+public struct SambaBridge {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SambaBridgeTests/SambaBridgeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SambaBridge
+
+final class SambaBridgeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SambaBridge().text, "Hello, World!")
+    }
+}
